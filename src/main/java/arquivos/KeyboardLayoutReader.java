5d7cc080480957d8f3121b2783e8e85993954f67
Priscila93 <priscila.dair@uniriotec.br>
2016-12-01 17:55:36 -0200

    Update KeyboardLayoutReader.java

--- src/main/java/arquivos/KeyboardLayoutReader.java
+++ src/main/java/arquivos/KeyboardLayoutReader.java
@@ -1,77 +1,67 @@
-
 package arquivos;
 
-import Teclado.Line;
-import Teclado.KeyboardLayout;
-import Teclado.KeyboardLayoutList;
 import java.io.File;
-import java.io.IOException;
-import org.w3c.dom.Document;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.SAXException;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
+
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-/**
- *
- *  Classe que lê o arquivo xml e guarda o conteúdo lido
- * 
- *  @author marcella e priscila
- */
-public class KeyboardLayoutReader
-{
-    /** 
-    *   Lê arquivo xml e guarda o conteúdo lido
-    */
-    public KeyboardLayoutList loadFromFile(String string) throws SAXException, IOException, ParserConfigurationException
-    {
-	KeyboardLayoutList listaTiposTeclado = new KeyboardLayoutList();
+import Teclado.KeyboardLayout;
+import Teclado.KeyboardLayoutList;
+import Teclado.Linha;
 
-	try
-        {
-            //abertura e leitura do arquivo xml
-            File file = new File("KeyboardLayouts.xml");
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document documento = documentBuilder.parse(file);
-            documento.getDocumentElement().normalize();
-            NodeList objetos = documento.getElementsByTagName("layout");
-            
-            //Prepara e adiciona um teclado à lista
-            for (int contadorObjetos = 0; contadorObjetos < objetos.getLength(); contadorObjetos++)
-            {
-                Node objeto = objetos.item(contadorObjetos);
-		if (objeto.getNodeType() == Node.ELEMENT_NODE)
-                {
-                    KeyboardLayout tipoTeclado = new KeyboardLayout();
-                    Element elemento = (Element) objeto;
-                    tipoTeclado.setModel(elemento.getAttribute("model"));
+public class KeyboardLayoutReader {
 
-                    for (int contadorLinhasLayout = 0; contadorLinhasLayout < 3; contadorLinhasLayout++)
-                    {
-			Line linha = new Line();
-			String caracteres = elemento.getElementsByTagName("line").item(contadorLinhasLayout).getTextContent();
-			linha.setLinhaCaracteres(caracteres);
-			tipoTeclado.addLine(linha);
-        		String offset = "" + elemento.getElementsByTagName("line").item(contadorLinhasLayout).getAttributes().getNamedItem("offset");
-                        
-			if (!offset.equals("null"))
-                        {
-                            offset = offset.substring(8, offset.length() - 1);
-                            Double cont = Double.parseDouble(offset);
-                            linha.setOffset(cont);
-                        }
-                        
-			listaTiposTeclado.addLayouts(tipoTeclado);
-                    }
+	/**
+	 * Carrega os teclados do xml
+	 */
+	public KeyboardLayoutList loadFromFile(String filePath) {
+
+		KeyboardLayoutList keyboardLayoutList = new KeyboardLayoutList();
+
+		try {
+			File fXmlFile = new File(filePath);
+
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+
+			NodeList nList = doc.getElementsByTagName("layout");
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					KeyboardLayout kl = new KeyboardLayout();
+					Element eElement = (Element) nNode;
+					kl.setModelo(eElement.getAttribute("model"));
+
+					for (int i = 0; i < 3; i++) {
+
+						Linha line = new Linha();
+						String content = eElement.getElementsByTagName("line").item(i).getTextContent();
+						line.setLetras(content);
+						kl.adicionarLinha(line);
+						String offset = ""
+								+ eElement.getElementsByTagName("line").item(i).getAttributes().getNamedItem("offset");
+						if (!offset.equals("null")) {
+							offset = offset.substring(8, offset.length() - 1);
+							Double cont = Double.parseDouble(offset);
+							line.setOffset(cont);
+						}
+
+					}
+					keyboardLayoutList.adiciona(kl);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-            }
-	} catch (Exception e){
-            e.printStackTrace();
+		return keyboardLayoutList;
 	}
-	return listaTiposTeclado;
-    }
 }
